d3c84d86bdfc1c79decf54da49d3890cf2c801f6
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-09-13 11:50:21 -0700

    Copy solution files from chatterbox-client

---
+++ client/env/config.js
@@ -0,0 +1,8 @@
+// All this is doing is inserting the parse API keys into every $.ajax
+// request that you make so you don't have to.
+
+// Put your parse application keys here!
+$.ajaxPrefilter(function (settings, _, jqXHR) {
+  jqXHR.setRequestHeader('X-Parse-Application-Id', '2745f6eedad1770c6ebaf03f8a97cf0cc2f66706');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', '4f44a6835e581124936858b658e8ea99e278d371');
+});
